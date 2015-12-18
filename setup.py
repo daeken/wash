d619f5734bac83d9ae383c3dcf3c39e6239a47e7
Cody Brocious <cody.brocious@gmail.com>
2015-12-18 00:27:28 -0700

    Bumped version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='WASH',
-		version='0.1', 
+		version='0.2', 
 		description='Wolfram|Alpha interactive shell', 
 		author='Cody Brocious', 
 		author_email='cody.brocious@gmail.com', 
