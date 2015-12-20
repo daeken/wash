2f5dccf89c208acbaa2b12152c04f66b2d8bb707
Cody Brocious <cody.brocious@gmail.com>
2015-12-20 12:23:56 -0700

    Added HTML and Markdown workbook output.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
 
 setup(name='WASH',
-		version='1.2', 
+		version='1.3', 
 		description='Wolfram|Alpha interactive shell', 
 		author='Cody Brocious', 
 		author_email='cody.brocious@gmail.com', 
 		scripts=['wash'], 
 		install_requires=[
 			'requests',
-			'beautifulsoup4'
+			'beautifulsoup4', 
+			'mako'
 		], 
 		license='CC0', 
 		url='https://pypi.python.org/pypi/WASH'
