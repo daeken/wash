934729ed0ffa394d43ae64b78a0233804a07b648
Cody Brocious <cody.brocious@gmail.com>
2015-12-20 11:07:53 -0700

    Bumped version, removed lxml dep.

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from distutils.core import setup
 
 setup(name='WASH',
-		version='1.1', 
+		version='1.2', 
 		description='Wolfram|Alpha interactive shell', 
 		author='Cody Brocious', 
 		author_email='cody.brocious@gmail.com', 
 		scripts=['wash'], 
 		install_requires=[
 			'requests',
-			'beautifulsoup4', 
-			'lxml'
+			'beautifulsoup4'
 		], 
 		license='CC0', 
 		url='https://pypi.python.org/pypi/WASH'
