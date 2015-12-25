c97fb6124fbdf4419525f6ace50b54066f8da47c
Cody Brocious <cody.brocious@gmail.com>
2015-12-25 12:58:31 -0700

    Switched to entry_points installation method, to work on Windows.

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='WASH',
-		version='1.5', 
+		version='1.6', 
 		description='Wolfram|Alpha interactive shell', 
 		author='Cody Brocious', 
 		author_email='cody.brocious@gmail.com', 
-		scripts=['wash'], 
+		py_modules=['wash'], 
+		entry_points=dict(
+			console_scripts=[
+				'wash = wash:main'
+			]
+		), 
 		install_requires=[
 			'requests',
 			'beautifulsoup4', 
